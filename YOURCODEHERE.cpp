39eae125311684ca167bd66d9178b19dd67b73ba
Avie Vasantlal <avasantlal@gmail.com>
2025-05-07 23:12:25 -0400

    i made a table

--- YOURCODEHERE.cpp
+++ YOURCODEHERE.cpp
@@ -14,6 +14,52 @@
 #include "431project.h"
 
 
+int count = 0l
+
+//latency tables
+
+int l1_latency_table(int size_kb, int ways) {
+    int base_latency;
+
+    // Base latency for direct-mapped (1-way)
+    if      (size_kb == 8)  base_latency = 1;
+    else if (size_kb == 16) base_latency = 2;
+    else if (size_kb == 32) base_latency = 3;
+    else if (size_kb == 64) base_latency = 4;
+    else return -1;
+
+    // Associativity adjustment
+    if      (ways == 1) return base_latency;
+    else if (ways == 2) return base_latency + 1;
+    else if (ways == 4) return base_latency + 2;
+    else return -1; // invalid associativity
+}
+
+int ul2_latency_table(int size_kb, int ways) {
+    int base_latency;
+
+    // Base latency (for 4-way set associative)
+    if      (size_kb == 128)  base_latency = 7;
+    else if (size_kb == 256)  base_latency = 8;
+    else if (size_kb == 512)  base_latency = 9;
+    else if (size_kb == 1024) base_latency = 10;
+    else if (size_kb == 2048) base_latency = 11;
+    else return -1;  // invalid size
+
+    int offset;  // invalid by default
+
+    // Associativity adjustment
+    if      (ways == 1)   offset = -2;
+    else if (ways == 2)   offset = -1;
+    else if (ways == 4)   offset = 0;
+    else if (ways == 8)   offset = 1;
+    else if (ways == 16)  offset = 2;
+    else return -1;  // invalid associativity
+
+    return base_latency + offset;
+}
+
+
 
 
 /*
